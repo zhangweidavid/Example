7dcc816789ad02c82766dbc44a3c4f7353e7408f
wei.zw <zhangwei_david@6163.com>
2018-03-14 17:32:12 +0800

    misc:test

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,12 +1,24 @@
 package demo;
 
+import demo.flux.EchoHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
+		return RouterFunctions.route(RequestPredicates.GET("/echo"), echoHandler::echo);
 	}
 }
