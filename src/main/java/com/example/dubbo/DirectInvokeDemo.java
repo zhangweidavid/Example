38821b4ed01422a8744e2dc124e8d20825c5e8b9
wei.zw <zhangwei_david@6163.com>
2018-03-22 23:23:35 +0800

    misc:test

---
+++ src/main/java/com/example/dubbo/DirectInvokeDemo.java
@@ -0,0 +1,24 @@
+package com.example.dubbo;
+
+
+
+public class DirectInvokeDemo {
+
+    public static void main(String[] args) {
+//        ApplicationConfig ac = new ApplicationConfig();
+//        ac.setName("test");
+//        RegistryConfig registryConfig = new RegistryConfig();
+//        registryConfig.setAddress("N/A");
+//        ac.setRegistry(registryConfig);
+//        ReferenceConfig<GenericService> referenceConfig = new ReferenceConfig<>();
+//        referenceConfig.setGeneric(true);
+//        referenceConfig.setInterface("com.netease.kaola.promotion.compose.service.PromotionForenoticePriceCompose");
+//        referenceConfig.setVersion("1.0");
+//        referenceConfig.setUrl("dubbo://10.165.125.189:20886");
+//        referenceConfig.setGroup("hst_test4");
+//        referenceConfig.setApplication(ac);
+//        GenericService genericService = referenceConfig.get();
+//        Object object = genericService.$invoke("queryFornoticePrice", new String[]{"com.netease.kaola.promotion.compose.params.ForenoticePriceParam"}, new Object[]{null});
+//        System.out.println(object);
+    }
+}
